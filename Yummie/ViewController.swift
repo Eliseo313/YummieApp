568b7cf64b400e4a3ee788f2e35e6c7096af5325
Eliseo313 <elicarortix58@gmail.com>
2024-06-26 22:30:38 -0600

    primer commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Eliseo Adrian Cardenas Ortiz on 26/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
