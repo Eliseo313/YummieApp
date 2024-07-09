55bdb09941d0a298bfd32d3f63bd2bf2a51c7056
Eliseo313 <elicarortix58@gmail.com>
2024-07-09 08:42:50 -0600

    categorias de comida terminada

--- Yummie/Views/Home/HomeViewController.swift
+++ Yummie/Views/Home/HomeViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
+    var categories: [DishCategory] = [
+        .init(id: "id1", name: "Comida Mexicana", image: "https://picsum.photos/100/200"),
+        .init(id: "id2", name: "Comida Italiana", image: "https://picsum.photos/100/200"),
+        .init(id: "id3", name: "Comida China", image: "https://picsum.photos/100/200"),
+        .init(id: "id4", name: "Comida Tailandesa", image: "https://picsum.photos/100/200"),
+        .init(id: "id5", name: "Comida Japonesa", image: "https://picsum.photos/100/200")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
-        title = "Yummie"
-    }
-
+        //title = "Yummie"
+        registerCells()
+     }
     
+    private func registerCells(){
+        categoryCollectionView.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil),forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+    }
+}
 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as! CategoryCollectionViewCell
+        cell.setup(category: categories[indexPath.row])
+        return cell
+    }
 }
