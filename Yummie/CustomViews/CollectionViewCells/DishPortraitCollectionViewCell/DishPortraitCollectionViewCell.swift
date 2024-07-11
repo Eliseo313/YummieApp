aa7b607cb3d9be96f41878d315432f9ff0824828
Eliseo313 <elicarortix58@gmail.com>
2024-07-10 22:48:26 -0600

    platillos populares terminados

---
+++ Yummie/CustomViews/CollectionViewCells/DishPortraitCollectionViewCell/DishPortraitCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DishPortraitCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Eliseo Adrian Cardenas Ortiz on 09/07/24.
+//
+
+import UIKit
+
+class DishPortraitCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Yummie/Models/Dish.swift
@@ -0,0 +1,8 @@
+//
+//  Dish.swift
+//  Yummie
+//
+//  Created by Eliseo Adrian Cardenas Ortiz on 10/07/24.
+//
+
+import Foundation
