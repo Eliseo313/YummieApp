4a2492132590404e0d9553bc68629c207d8f87ed
Eliseo313 <elicarortix58@gmail.com>
2024-07-14 21:21:09 -0600

    ventana detalles de platillo creada

--- Yummie/Views/DishDetail/DishDetailViewController.swift
+++ Yummie/Views/DishDetail/DishDetailViewController.swift
@@ -15,11 +15,22 @@ class DishDetailViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var descriptionLbl: UILabel!
     
+    var dish : Dish!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
  
+        populateView()
     }
 
+    private func populateView(){
+        dishImageView.kf.setImage(with: dish.image?.asUrl)
+        titleLbl.text = dish.name
+        descriptionLbl.text = dish.description
+        caloriesLbl.text = dish.formattedCalories
+        
+    }
+    
     @IBAction func ordenarBtnClicked(_ sender: UIButton) {
         
         
